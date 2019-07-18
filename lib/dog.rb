c10c3d90da5ac66ff60c93b62758ab13b48c9ac4
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-18 16:10:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,6 +35,13 @@ class Dog
    end.first  
   end 
   
+  def self.find_by_id(id)
+   sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1;"
+   DB[:conn].execute(sql, id).collect do |row|
+     self.new_from_db(row)
+   end.first  
+  end 
+  
   def update
     sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
