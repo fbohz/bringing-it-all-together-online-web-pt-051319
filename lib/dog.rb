01593dbf5c1edfd1040cea25928c3cc309b6268e
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-18 16:08:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,6 +30,11 @@ class Dog
   
   def self.find_by_name(name)
    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1;"
+   DB[:conn].execute(sql, name).collect do |row|
+     self.new_from_db(row)
+   end.first  
   end 
+  
+  
   
 end   
